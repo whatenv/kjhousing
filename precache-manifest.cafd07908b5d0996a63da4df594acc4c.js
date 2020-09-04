022aa023ade0c9bd882653d4d5754ced992c6092
whatenv <histerria@gmail.com>
2020-09-04 12:51:29 +0900

    Updates

---
+++ precache-manifest.cafd07908b5d0996a63da4df594acc4c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f75a42d5780a2934e669453b4cfcb1c4",
+    "url": "/kjhousing/index.html"
+  },
+  {
+    "revision": "453aebdc1a8be5e0ba5c",
+    "url": "/kjhousing/static/css/main.923c1028.chunk.css"
+  },
+  {
+    "revision": "66919851e47136d0f98e",
+    "url": "/kjhousing/static/js/2.1370c5b4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kjhousing/static/js/2.1370c5b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "453aebdc1a8be5e0ba5c",
+    "url": "/kjhousing/static/js/main.e85cbf8b.chunk.js"
+  },
+  {
+    "revision": "1272d52d583a83daaebe",
+    "url": "/kjhousing/static/js/runtime-main.95bc5e9d.js"
+  }
+]);
