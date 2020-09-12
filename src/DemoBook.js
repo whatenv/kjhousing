1cd98a68b61e9586c06215d30372e4e3d73d3229
whatenv <histerria@gmail.com>
2020-09-12 12:19:25 +0900

    Trivia

--- src/DemoBook.js
+++ src/DemoBook.js
@@ -131,287 +131,287 @@ class DemoBook extends React.Component {
           <Page bg={"/images/s.png"}></Page>
           <Page bg={"/images/s_pattern.jpeg"}></Page>
           <Page bg={"/images/s/1.jpg"}>
-            내추럴오크
+            원목마루 S시리즈 내추럴오크
           </Page>
           <Page bg={"/images/s/2.jpg"}>
-            티크
+            원목마루 S시리즈 티크
           </Page>
           <Page bg={"/images/s/3.jpg"}>
-            월넛
+            원목마루 S시리즈 월넛
           </Page>
           <Page bg={"/images/s/4.jpg"}>
-            멀바우
+            원목마루 S시리즈 멀바우
           </Page>
           <Page bg={"/images/s/5.jpg"}>
-            두시에
+            원목마루 S시리즈 두시에
           </Page>
           <Page bg={"/images/s/6.jpg"}>
-            이로코
+            원목마루 S시리즈 이로코
           </Page>
           <Page bg={"/images/s/7.jpg"}>
-            카바랜덤
+            원목마루 S시리즈 카바랜덤
           </Page>
           <Page bg={"/images/s/8.jpg"}>
-            버찌
+            원목마루 S시리즈 버찌
           </Page>
           <Page bg={"/images/s/9.jpg"}>
-            러스틱티크
+            원목마루 S시리즈 러스틱티크
           </Page>
           <Page bg={"/images/s/10.jpg"}>
-            러스틱멀바우
+            원목마루 S시리즈 러스틱멀바우
           </Page>
           <Page bg={"/images/m.png"}></Page>
           <Page bg={"/images/m_pattern.jpeg"}></Page>
           <Page bg={"/images/m/1.jpg"}>
-            내추럴오크
+            원목마루 M시리즈 내추럴오크
           </Page>
           <Page bg={"/images/m/2.jpg"}>
-            샌드브라운
+            원목마루 M시리즈 원목마루 M시리즈 샌드브라운
           </Page>
           <Page bg={"/images/m/3.jpg"}>
-            브라운토치
+            원목마루 M시리즈 브라운토치
           </Page>
           <Page bg={"/images/m/4.jpg"}>
-            프렌치블랙
+            원목마루 M시리즈 프렌치블랙
           </Page>
           <Page bg={"/images/w.png"}></Page>
           <Page bg={"/images/w_pattern.jpeg"}></Page>
           <Page bg={"/images/w/1.jpg"}>
-            내추럴오크
+            원목마루 W시리즈 내추럴오크
           </Page>
           <Page bg={"/images/w/2.jpg"}>
-            티크
+            원목마루 W시리즈 티크
           </Page>
           <Page bg={"/images/w/3.jpg"}>
-            샌드브라운
+            원목마루 W시리즈 샌드브라운
           </Page>
           <Page bg={"/images/w/4.jpg"}>
-            스노우베이지
+            원목마루 W시리즈 스노우베이지
           </Page>
           <Page bg={"/images/w/5.jpg"}>
-            워시오크
+            원목마루 W시리즈 워시오크
           </Page>
           <Page bg={"/images/w/6.jpg"}>
-            브라운오크
+            원목마루 W시리즈 브라운오크
           </Page>
           <Page bg={"/images/w/7.jpg"}>
-            소프트그레이
+            원목마루 W시리즈 소프트그레이
           </Page>
           <Page bg={"/images/w/8.jpg"}>
-            월넛
+            원목마루 W시리즈 월넛
           </Page>
           <Page bg={"/images/w/9.jpg"}>
-            멀바우
+            원목마루 W시리즈 멀바우
           </Page>
           <Page bg={"/images/w/10.jpg"}>
-            프렌치블랙
+            원목마루 W시리즈 프렌치블랙
           </Page>
           <Page bg={"/images/w/11.jpg"}>
-            빈티지오크
+            원목마루 W시리즈 빈티지오크
           </Page>
           <Page bg={"/images/w/12.jpg"}>
-            빈티지샌드브라운
+            원목마루 W시리즈 빈티지샌드브라운
           </Page>
           <Page bg={"/images/w/13.jpg"}>
-            빈티지웨이브
+            원목마루 W시리즈 빈티지웨이브
           </Page>
           <Page bg={"/images/w/14.jpg"}>
-            러스틱티크
+            원목마루 W시리즈 러스틱티크
           </Page>
           <Page bg={"/images/w/15.jpg"}>
-            러스틱멀바우
+            원목마루 W시리즈 러스틱멀바우
           </Page>
           <Page bg={"/images/st.png"}></Page>
           <Page bg={"/images/st_pattern.jpeg"}></Page>
           <Page bg={"/images/st/1.jpg"}>
-            내추럴오크
+            원목마루 ST시리즈 내추럴오크
           </Page>
           <Page bg={"/images/st/2.jpg"}>
-            화이트워시오크N
+            원목마루 ST시리즈 화이트워시오크N
           </Page>
           <Page bg={"/images/st/3.jpg"}>
-            카키브라운
+            원목마루 ST시리즈 카키브라운
           </Page>
           <Page bg={"/images/st/4.jpg"}>
-            버건디로즈
+            원목마루 ST시리즈 버건디로즈
           </Page>
           <Page bg={"/images/st/5.jpg"}>
-            애쉬내추럴
+            원목마루 ST시리즈 애쉬내추럴
           </Page>
           <Page bg={"/images/st/6.jpg"}>
-            애쉬L.브라운
+            원목마루 ST시리즈 애쉬L.브라운
           </Page>
           <Page bg={"/images/st/7.jpg"}>
-            애쉬그레이
+            원목마루 ST시리즈 애쉬그레이
           </Page>
           <Page bg={"/images/st/8.jpg"}>
-            허니멀바우
+            원목마루 ST시리즈 허니멀바우
           </Page>
           <Page bg={"/images/st/9.jpg"}>
-            앤틱티크
+            원목마루 ST시리즈 앤틱티크
           </Page>
           <Page bg={"/images/st/10.jpg"}>
-            언더포레스트
+            원목마루 ST시리즈 언더포레스트
           </Page>
           <Page bg={"/images/st/11.jpg"}>
-            젠틀브라운
+            원목마루 ST시리즈 젠틀브라운
           </Page>
           <Page bg={"/images/st/12.jpg"}>
-            코지다크
+            원목마루 ST시리즈 코지다크
           </Page>
           <Page bg={"/images/b.png"}></Page>
           <Page bg={"/images/b_pattern.jpeg"}></Page>
           <Page bg={"/images/b/1.jpg"}>
-            내추럴오크
+            원목마루 B시리즈 내추럴오크
           </Page>
           <Page bg={"/images/b/2.jpg"}>
-            비치샌드
+            원목마루 B시리즈 비치샌드
           </Page>
           <Page bg={"/images/b/3.jpg"}>
-            브라운리버
+            원목마루 B시리즈 브라운리버
           </Page>
           <Page bg={"/images/k.png"}></Page>
           <Page bg={"/images/k_pattern.jpeg"}></Page>
           <Page bg={"/images/k/1.jpg"}>
-            내추럴오크
+            원목마루 K시리즈 내추럴오크
           </Page>
           <Page bg={"/images/k/2.jpg"}>
-            차밍마룬
+            원목마루 K시리즈 차밍마룬
           </Page>
           <Page bg={"/images/k/3.jpg"}>
-            모던차콜
+            원목마루 K시리즈 모던차콜
           </Page>
           <Page bg={"/images/gang.png"}></Page>
           <Page bg={"/images/gang_intro.jpeg"}></Page>
           <Page bg={"/images/gang_pattern.jpeg"}>
           </Page>
           <Page bg={"/images/gang/1.jpg"}>
-            내추럴티크
+            강마루 내추럴티크
           </Page>
           <Page bg={"/images/gang/2.jpg"}>
-            메이플
+            강마루 메이플
           </Page>
           <Page bg={"/images/gang/3.jpg"}>
-            신브라운오크
+            강마루 신브라운오크
           </Page>
           <Page bg={"/images/gang/4.jpg"}>
-            블랙오크
+            강마루 블랙오크
           </Page>
           <Page bg={"/images/gang/5.jpg"}>
-            오크내추럴
+            강마루 오크내추럴
           </Page>
           <Page bg={"/images/gang/6.jpg"}>
-            워시오크
+            강마루 워시오크
           </Page>
           <Page bg={"/images/gang/7.jpg"}>
-            마일드오크
+            강마루 마일드오크
           </Page>
           <Page bg={"/images/gang/8.jpg"}>
-            화이트오크
+            강마루 화이트오크
           </Page>
           <Page bg={"/images/gang/9.jpg"}>
-            화이트워시오크
+            강마루 화이트워시오크
           </Page>
           <Page bg={"/images/gang/10.jpg"}>
-            그레이워시오크
+            강마루 그레이워시오크
           </Page>
           <Page bg={"/images/gang/11.jpg"}>
-            시에라블랙
+            강마루 시에라블랙
           </Page>
           <Page bg={"/images/gang/12.jpg"}>
-            시에라샌드
+            강마루 시에라샌드
           </Page>
           <Page bg={"/images/gang/13.jpg"}>
-            시에라클라우드
+            강마루 시에라클라우드
           </Page>
           <Page bg={"/images/gang/14.jpg"}>
-            토프그레이
+            강마루 토프그레이
           </Page>
           <Page bg={"/images/gangw_pattern.jpeg"}>
           </Page>
           <Page bg={"/images/gang_w/1.jpg"}>
-            와이드라이트
+            강마루 와이드라이트
           </Page>
           <Page bg={"/images/gang_w/2.jpg"}>
-            와이드모던그레이
+            강마루 와이드모던그레이
           </Page>
           <Page bg={"/images/gang_w/3.jpg"}>
-            와이드블랙
+            강마루 와이드블랙
           </Page>
           <Page bg={"/images/gang_w/4.jpg"}>
-            와이드빈티지다크
+            강마루 와이드빈티지다크
           </Page>
           <Page bg={"/images/gang_w/5.jpg"}>
-            와이드빈티지아이보리
+            강마루 와이드빈티지아이보리
           </Page>
           <Page bg={"/images/gang_w/6.jpg"}>
-            와이드스모크
+            강마루 와이드스모크
           </Page>
           <Page bg={"/images/gang_w/7.jpg"}>
-            와이드앤틱
+            강마루 와이드앤틱
           </Page>
           <Page bg={"/images/gang_w/8.jpg"}>
-            와이드오크
+            강마루 와이드오크
           </Page>
           <Page bg={"/images/gang_w/9.jpg"}>
-            와이드크림
+            강마루 와이드크림
           </Page>
           <Page bg={"/images/gang_w/10.jpg"}>
-            와이드화이트그레이
+            강마루 와이드화이트그레이
           </Page>
           <Page bg={"/images/gang_w/11.jpg"}>
-            와이드빈티지프로스트
+            강마루 와이드빈티지프로스트
           </Page>
           <Page bg={"/images/gang_w/12.jpg"}>
-            와이드오팔그레이
+            강마루 와이드오팔그레이
           </Page>
           <Page bg={"/images/gangn_pattern.jpeg"}></Page>
           <Page bg={"/images/gang_n/1.jpg"}>
-            리얼블랙
+            노블강마루 리얼블랙
           </Page>
           <Page bg={"/images/gang_n/2.jpg"}>
-            리얼시티
+            노블강마루 리얼시티
           </Page>
           <Page bg={"/images/gang_n/3.jpg"}>
-            리얼애쉬
+            노블강마루 리얼애쉬
           </Page>
           <Page bg={"/images/gang_n/4.jpg"}>
-            리얼오크
+            노블강마루 리얼오크
           </Page>
           <Page bg={"/images/gang_n/5.jpg"}>
-            리얼카카오
+            노블강마루 리얼카카오
           </Page>
           <Page bg={"/images/gang_n/6.jpg"}>
-            리얼티크
+            노블강마루 리얼티크
           </Page>
           <Page bg={"/images/gang_n/7.jpg"}>
-            마인미스트
+            노블강마루 마인미스트
           </Page>
           <Page bg={"/images/gang_n/8.jpg"}>
-            마인스웨이드
+            노블강마루 마인스웨이드
           </Page>
           <Page bg={"/images/gang_n/9.jpg"}>
-            마인워시
+            노블강마루 마인워시
           </Page>
           <Page bg={"/images/cheon.jpeg"}></Page>
           <Page bg={"/images/cheon_pattern.jpeg"}></Page>
           <Page bg={"/images/cheon/1.jpg"}>
-            애쉬아이보리
+            천연마루 애쉬아이보리
           </Page>
           <Page bg={"/images/cheon/2.jpg"}>
-            애쉬베이지
+            천연마루 애쉬베이지
           </Page>
           <Page bg={"/images/cheon/3.jpg"}>
-            애쉬내추럴
+            천연마루 애쉬내추럴
           </Page>
           <Page bg={"/images/cheon/4.jpg"}>
-            애쉬브라운
+            천연마루 애쉬브라운
           </Page>
           <Page bg={"/images/cheon/5.jpg"}>
-            애쉬모카
+            천연마루 애쉬모카
           </Page>
           <Page bg={"/images/cheon/6.jpg"}>
-            애쉬다크
+            천연마루 애쉬다크
           </Page>
           <Page bg={"/images/hwang.jpeg"}></Page>
           <Page bg={"/images/hwang_intro.jpeg"}></Page>
@@ -432,26 +432,26 @@ class DemoBook extends React.Component {
           <PageCover>www.kujunghousing.com<br></br>blog.naver.com/kujunghousing<br></br>031-576-4003</PageCover>
         </HTMLFlipBook>
 
-        <button className="left" type="button">
+        <div className="left" type="button" onClick={this.prevButtonClick}>
           <i className="fa fa-angle-left"></i>
-        </button>
-        <button className="right" type="button">
+        </div>
+        <div className="right" type="button" onClick={this.nextButtonClick}>
           <i className="fa fa-angle-right"></i>
-        </button>
+        </div>
         <div className="index">
           <div className="indexchild" onClick={() => this.movePage(0)}><span>표지</span></div>
           <div className="indexchild" onClick={() => this.movePage(1)}> 목차</div>
-          <div className="indexchild" onClick={() => this.movePage(5)}>원목마루 S</div>
-          <div className="indexchild" onClick={() => this.movePage(17)}>원목마루 M</div>
-          <div className="indexchild" onClick={() => this.movePage(23)}>원목마루 W</div>
-          <div className="indexchild" onClick={() => this.movePage(39)}>원목마루 ST</div>
-          <div className="indexchild" onClick={() => this.movePage(53)}>원목마루 B</div>
-          <div className="indexchild" onClick={() => this.movePage(59)}>원목마루 K</div>
+          <div className="indexchild" onClick={() => this.movePage(5)}>원목마루 S시리즈</div>
+          <div className="indexchild" onClick={() => this.movePage(17)}>원목마루 M시리즈</div>
+          <div className="indexchild" onClick={() => this.movePage(23)}>원목마루 W시리즈</div>
+          <div className="indexchild" onClick={() => this.movePage(39)}>원목마루 ST시리즈</div>
+          <div className="indexchild" onClick={() => this.movePage(53)}>원목마루 B시리즈</div>
+          <div className="indexchild" onClick={() => this.movePage(59)}>원목마루 K시리즈</div>
           <div className="indexchild" onClick={() => this.movePage(65)}>강마루</div>
           <div className="indexchild" onClick={() => this.movePage(71)}>강마루 와이드</div>
           <div className="indexchild" onClick={() => this.movePage(93)}>노블 강마루</div>
           <div className="indexchild" onClick={() => this.movePage(105)}>천연마루</div>
-          <div className="indexchild" onClick={() => this.movePage(111)}>황토풀</div>
+          <div className="indexchild" onClick={() => this.movePage(111)}>친환경 접착제</div>
           <div className="indexchild" onClick={() => this.movePage(115)}>오시는길</div>
         </div>
 
@@ -475,17 +475,17 @@ class DemoBook extends React.Component {
           <div className="closebtn" onClick={this.closeNav}>X</div>
           <div onClick={() => this.movePage(0)}>표지</div>
           <div onClick={() => this.movePage(1)}>목차</div>
-          <div onClick={() => this.movePage(5)}>원목마루 S</div>
-          <div onClick={() => this.movePage(17)}>원목마루 M</div>
-          <div onClick={() => this.movePage(23)}>원목마루 W</div>
-          <div onClick={() => this.movePage(39)}>원목마루 ST</div>
-          <div onClick={() => this.movePage(53)}>원목마루 B</div>
-          <div onClick={() => this.movePage(59)}>원목마루 K</div>
+          <div onClick={() => this.movePage(5)}>원목마루 S시리즈</div>
+          <div onClick={() => this.movePage(17)}>원목마루 M시리즈</div>
+          <div onClick={() => this.movePage(23)}>원목마루 W시리즈</div>
+          <div onClick={() => this.movePage(39)}>원목마루 ST시리즈</div>
+          <div onClick={() => this.movePage(53)}>원목마루 B시리즈</div>
+          <div onClick={() => this.movePage(59)}>원목마루 K시리즈</div>
           <div onClick={() => this.movePage(65)}>강마루</div>
           <div onClick={() => this.movePage(81)}>강마루 와이드</div>
           <div onClick={() => this.movePage(93)}>노블 강마루</div>
           <div onClick={() => this.movePage(105)}>천연마루</div>
-          <div onClick={() => this.movePage(111)}>황토풀</div>
+          <div onClick={() => this.movePage(111)}>친환경 접착제</div>
           <div onClick={() => this.movePage(115)}>오시는길</div>
         </div>
       </div >
